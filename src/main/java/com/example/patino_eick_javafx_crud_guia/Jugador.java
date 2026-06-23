2a0e0800e6bd4a814fc3ef8da9d8dff7ed8afe7a
TheErickYT10 <erickpatino777@gmail.com>
2026-06-23 15:51:57 -0500

    CLASE JUGADOR

--- src/main/java/com/example/patino_eick_javafx_crud_guia/Jugador.java
+++ src/main/java/com/example/patino_eick_javafx_crud_guia/Jugador.java
@@ -1,4 +1,112 @@
 package com.example.patino_eick_javafx_crud_guia;
 
 public class Jugador {
+
+    private int id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private String posicion;
+    private String piernaHabil;
+    private String equipo;
+    private String observaciones;
+
+    public Jugador() {
+    }
+
+    public Jugador(int id, String cedula, String nombre, String apellido, int edad, String correo, String posicion, String piernaHabil, String equipo, String observaciones) {
+
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.posicion = posicion;
+        this.piernaHabil = piernaHabil;
+        this.equipo = equipo;
+        this.observaciones = observaciones;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getPiernaHabil() {
+        return piernaHabil;
+    }
+
+    public void setPiernaHabil(String piernaHabil) {
+        this.piernaHabil = piernaHabil;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 }
